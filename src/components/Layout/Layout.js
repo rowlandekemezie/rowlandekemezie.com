a8a388dd285ecc402ba0990184adde05f52437c8
Rowland Ekemezie <15085641+rowlandekemezie@users.noreply.github.com>
2019-04-02 03:45:27 +0100

    - Add footer component (#16)
    - Nuke copy right component
    - Add subscription with tiny letter

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Footer from 'components/Footer';
 import styles from './Layout.module.scss';
 
 const Layout = ({
@@ -8,19 +9,22 @@ const Layout = ({
   description,
   keywords
 }) => (
-  <div className={styles.layout}>
-    <Helmet
-      title={title}
-      meta={[
-        { name: 'description', content: description },
-        { name: 'keywords', content: (keywords || []).join() }
-      ]}
-    >
-      <html lang="en" />
-      <noscript>This site runs best with JavaScript enabled.</noscript>
-    </Helmet>
-    {children}
-  </div>
+  <>
+    <div className={styles.layout}>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: description },
+          { name: 'keywords', content: (keywords || []).join() }
+        ]}
+      >
+        <html lang="en" />
+        <noscript>This site runs best with JavaScript enabled.</noscript>
+      </Helmet>
+      {children}
+    </div>
+    <Footer />
+  </>
 );
 
 export default Layout;
