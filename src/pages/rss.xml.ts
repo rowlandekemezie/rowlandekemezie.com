5116a3bf38f775ad6b47ccf6309f7758f41594cb
Rowland I. Ekemezie <15085641+rowlandekemezie@users.noreply.github.com>
2026-05-02 09:23:31 -0700

    Add RSS, sitemap, robots, and output checks

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+import { getPostExcerpt, getPostPath, getPublishedPosts, parsePostDate } from '../lib/content';
+import { site } from '../lib/site';
+
+export async function GET(context: { site?: URL }) {
+  const posts = await getPublishedPosts();
+
+  return rss({
+    title: site.title,
+    description: site.description,
+    site: context.site ?? site.url,
+    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: getPostExcerpt(post),
+      link: getPostPath(post),
+      pubDate: parsePostDate(post.data.date),
+      categories: [post.data.category, ...post.data.tags],
+    })),
+  });
+}
