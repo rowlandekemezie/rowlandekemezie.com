b820a7a2a30015be7344e619a66555c6d5ac04bc
Rowland I. Ekemezie <15085641+rowlandekemezie@users.noreply.github.com>
2026-05-02 09:44:26 -0700

    Preserve markdown heading links and document compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 export default defineConfig({
   image: {
     service: passthroughImageService(),
+  },
+  markdown: {
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
   },
   site: 'https://rowlandekemezie.com',
 });
